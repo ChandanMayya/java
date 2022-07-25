a2114e47edb5fcb1b8f539ed22733470b7026078
ChandanMayya <chandanmogral@gmail.com>
2022-07-25 15:01:51 +0530

    inheri3 18/7

--- inherit_demo.java
+++ inherit_demo.java
@@ -1,23 +1,33 @@
 class shape{
-    double height,width;
-    shape(){
-        height = 20;
-        width = 20;
+    private double height,width;
+  /*  shape(double h,double w){
+        height = h;
+        width = w;
         System.out.println("Im super cls");
+    }*/
+    double getWidth(){
+        return width;
+    }
+    double getHeight(){
+        return height;
+    }
+    void setWidth(double w){
+        width=w;
+    }
+    void setHeight(double h){
+        height=h;
     }
     void print_sup(){
         System.out.println("Im in the super class: shape");
     }
 }
 class triangle extends shape{
+    /*triangle(double h, double w) {
+        super(h, w);
+    }*/
     String s;
-    triangle(){
-        height=10;
-        width=10;
-        System.out.println("Im sub cls");
-    }
     double area(){
-        return height*width/2;
+        return getHeight()*getWidth()/2;
     }
     void print_sub(){
         System.out.println("Im in the sub class: triangle");
@@ -26,6 +36,8 @@ class triangle extends shape{
 public class inherit_demo{
     public static void main(String[] args) {
         triangle t1 = new triangle();
+        t1.setHeight(20);
+        t1.setWidth(20);
         t1.print_sup();
         t1.print_sub();
         System.out.println("Area is "+t1.area() );
