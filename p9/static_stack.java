de597a7f88a0a0e4a8acfb9fb2cbfd0365c6b39c
ChandanMayya <chandanmogral@gmail.com>
2022-08-08 14:29:20 +0530

    8aug/2

--- p9/static_stack.java
+++ p9/static_stack.java
@@ -1,5 +1,37 @@
 package p9;
 
-public class static_stack {
+public class static_stack implements stk_intrfc {
     
+    private char[] data;
+    private int top;
+    
+    static_stack( int size){
+        data=new char[size];
+        top=0;
+    }
+
+    public void push(char ch){
+        if(isFull()){
+            System.out.println("Stack Full");
+            return;
+        }
+        data[top]=ch;
+        top++;
+        }
+    }
+    public char pop()
+    {
+        if(isEmpty()){
+            System.out.println("Stack Empty");
+            return (char) 0;
+        }
+        top--;
+        return data[top];
+    }
+    public boolean isEmpty(){
+        return top==0;
+    }
+    public boolean isFull(){
+        return top==data.length;
+    }
 }
